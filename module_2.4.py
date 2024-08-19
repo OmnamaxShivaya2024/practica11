b2a0841c0c700a69d84a1d505e55d3c58b0e6f82
Людмила Кузнецова <ananim2023@list.ru>
2024-08-19 09:08:53 +0300

    Add files via upload

---
+++ module_2.4.py
@@ -0,0 +1,47 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in numbers:
+     is_prim =True
+     if i == 1:
+          continue
+     for j in range(2, i):
+          if i % j == 0:
+               is_prim = False
+               break
+
+     if is_prim:
+          primes.append(i)
+          continue
+     else:
+         not_primes.append(i)
+print("primes", primes)
+print("not_primes", not_primes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
